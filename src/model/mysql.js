b5d8652575af85231e685893a4a225cc12eb693b
matheuszack <113534912+matheuszack@users.noreply.github.com>
2023-04-03 11:50:40 -0300

    Update mysql.js

--- src/model/mysql.js
+++ src/model/mysql.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql2');
 const conf = require('dotenv').config().parsed;
 
-const conn = mysql.createPool({
-    host: conf.HOSTNAME,
-    user: conf.USERNAME,
-    password: conf.PASSWORD,
-    database: conf.DATABASE,
-    port: conf.HOSTPORT
-}).promise();
+async function connectToDatabase() {
+  try {
+    const conn = await mysql.createPool({
+      host: conf.HOSTNAME,
+      user: conf.USERNAME,
+      password: conf.PASSWORD,
+      database: conf.DATABASE,
+      port: conf.DB_PORT
+    }).promise();
 
-module.exports = conn;
+    console.log('Conexão com o banco de dados estabelecida com sucesso');
+    return conn;
+  } catch (err) {
+    console.error('Erro ao conectar com o banco de dados', err);
+    throw err;
+  }
+}
+
+module.exports = connectToDatabase();
